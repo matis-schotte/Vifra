cd32c407b1115a30d6e1c3ee583f9e694cb7b7b8
Matis Schotte <dm26f1cab8aa26@ungeord.net>
2018-03-19 19:29:14 +0100

    Add Package.swift to support SPM

---
+++ Package.swift
@@ -0,0 +1,11 @@
+//
+//  Package.swift
+//  Vifra
+//
+//  Created by Matis Schotte on 19.03.18.
+//  Copyright © 2018 ungeord.net. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "Vifra", dependencies : [], exclude: ["VifraTests", "VifraExample"])
